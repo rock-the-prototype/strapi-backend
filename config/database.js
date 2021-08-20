22f0a904c24b0e6b5f3d3f0348fcdc183d96edef
AS <as@andre-schuenemann.de>
2021-08-20 22:26:21 +0200

    change db config

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', 'postgres13master'),
+        host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', 'strapidb'),
-        password: env('DATABASE_PASSWORD', '4C#e%3mMR#6a7cdVYFqHb'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
         ssl: false
       },
       options: {},
